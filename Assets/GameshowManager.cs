dc94df5768e947a40da4dd0c968833b62ad396bf
Matt Graber <grabermtw@gmail.com>
2023-04-08 22:32:41 -0400

    Tweaked opening text

--- Assets/GameshowManager.cs
+++ Assets/GameshowManager.cs
@@ -26,7 +26,7 @@ public class GameshowManager : MonoBehaviour
     private void Opening()
     {
         List<Dialogue> testDialogue = new List<Dialogue>();
-        testDialogue.Add(new Dialogue("Bestudo", "Welcome to the show!", dialogueSystem.wholeStageCam, AnimCategory.CorrectAnswer));
+        testDialogue.Add(new Dialogue("Bestudo", "Welcome to the show! I'm your host, Bestudo!", dialogueSystem.wholeStageCam, AnimCategory.CorrectAnswer));
         testDialogue.Add(new Dialogue("Bestudo", "So you want to be a realtor?", dialogueSystem.bestudoCam, AnimCategory.CorrectAnswer));
         testDialogue.Add(new Dialogue("Bestudo", "Well, you've got a lot of learning to do!", dialogueSystem.bestudoCam, AnimCategory.WrongAnswer));
         testDialogue.Add(new Dialogue("Bestudo", "So whattdya say? Wanna show your real estate smarts?", dialogueSystem.bestudoCam, AnimCategory.CorrectAnswer));
